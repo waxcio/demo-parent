ab3bf6f3658ebb4067c0d376f1a3738b21541b01
Manchi Leung <man-chi@central.tech>
2019-06-16 14:36:32 +0700

    added hello world

--- webstarter/src/main/java/io/waxc/demo/webstarter/WebstarterApplication.java
+++ webstarter/src/main/java/io/waxc/demo/webstarter/WebstarterApplication.java
@@ -1,8 +1,12 @@
 package io.waxc.demo.webstarter;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class WebstarterApplication {
 
@@ -10,4 +14,8 @@ public class WebstarterApplication {
         SpringApplication.run(WebstarterApplication.class, args);
     }
 
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello World";
+    }
 }
