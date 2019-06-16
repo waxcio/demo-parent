209ffd57f246b0b5d6f8e2dbd7e99a287150da8f
Manchi Leung <man-chi@central.tech>
2019-06-16 15:06:19 +0700

    added swagger2

--- webstarter/src/main/java/io/waxc/demo/webstarter/WebstarterApplication.java
+++ webstarter/src/main/java/io/waxc/demo/webstarter/WebstarterApplication.java
@@ -5,9 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 @SpringBootApplication
+@EnableSwagger2
 public class WebstarterApplication {
 
     public static void main(String[] args) {
@@ -17,5 +24,11 @@ public class WebstarterApplication {
     @RequestMapping(value = "/")
     public String hello() {
         return "Hello World";
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("io.waxc.demo.webstarter")).build();
     }
 }
