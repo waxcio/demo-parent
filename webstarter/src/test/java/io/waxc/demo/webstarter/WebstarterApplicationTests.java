af235edd891d65e3c3600f513c40bb3742bab2a8
Manchi Leung <man-chi@central.tech>
2019-06-16 14:32:13 +0700

    Initial commit

---
+++ webstarter/src/test/java/io/waxc/demo/webstarter/WebstarterApplicationTests.java
@@ -0,0 +1,16 @@
+package io.waxc.demo.webstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebstarterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
