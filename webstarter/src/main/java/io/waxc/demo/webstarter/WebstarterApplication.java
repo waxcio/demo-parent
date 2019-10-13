8c8c7adcbd01540fd9896039c8a824eb8f5dfb35
Manchi Leung <manchi@waxc.tech>
2019-10-13 18:19:00 +0700

    completed scheduler

--- webstarter/src/main/java/io/waxc/demo/webstarter/WebstarterApplication.java
+++ webstarter/src/main/java/io/waxc/demo/webstarter/WebstarterApplication.java
@@ -8,27 +8,23 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Locale;
-
 
 @RestController
 @SpringBootApplication
 @EnableSwagger2
+@EnableScheduling
 public class WebstarterApplication implements ApplicationRunner, WebMvcConfigurer {
 
     private static final Logger logger = LoggerFactory.getLogger(WebstarterApplication.class);
---
+++ webstarter/src/main/java/io/waxc/demo/webstarter/scheduler/Scheduler.java
@@ -0,0 +1,47 @@
+package io.waxc.demo.webstarter.scheduler;
+
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class Scheduler {
+
+    /*
+     *The following is a sample code that shows how to execute the task every minute starting at 9:00 AM and ending at 9:59 AM, every day
+     * https://docs.oracle.com/cd/E12058_01/doc/doc.1014/e12030/cron_expressions.htm
+     */
+    @Scheduled(cron = "0 18 18 * * ?")
+    public void cronJobSch() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("Java cron job expression:: " + strDate);
+    }
+
+    /*
+     *A sample code for executing a task on every second from the application startup is shown here −
+     */
+    @Scheduled(fixedRate = 1000)
+    public void fixedRateSch() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("Fixed Rate scheduler:: " + strDate);
+    }
+
+    /*
+     *    An example to execute the task for every second after 3 seconds from the application startup has been completed is shown below −
+     */
+    @Scheduled(fixedDelay = 1000, initialDelay = 3000)
+    public void fixedDelaySch() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("Fixed Delay scheduler:: " + strDate);
+    }
+}
